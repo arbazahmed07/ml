c4690e6731c96db96f2d7d237f21dea7b3adff5f
arbazahmed07 <mdarbazking7@gmail.com>
2026-05-13 19:30:18 +0530

    mlp added

--- kmeans.py
+++ kmeans.py
@@ -1,17 +1,30 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
 
-# Load data
-data = pd.read_csv("Iris.csv")
-X = data.iloc[:, :2]   # take only 2 features for easy plotting
+# Load inbuilt Iris dataset
+data = load_iris()
 
-# Model
-model = KMeans(n_clusters=3)
+# Take first 2 features
+X = data.data[:, :2]
+
+# K-Means model
+model = KMeans(n_clusters=3, random_state=42)
+
+# Train model
 model.fit(X)
 
-# Plot
-plt.scatter(X.iloc[:,0], X.iloc[:,1], c=model.labels_)
-plt.scatter(model.cluster_centers_[:,0], model.cluster_centers_[:,1])
+# Plot clusters
+plt.scatter(X[:,0], X[:,1], c=model.labels_)
+
+# Plot centroids
+plt.scatter(
+    model.cluster_centers_[:,0],
+    model.cluster_centers_[:,1]
+)
+
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
 plt.title("K-Means Clustering")
+
 plt.show()
--- sp.py
+++ sp.py
@@ -19,3 +19,34 @@ y_pred = model.predict(X)
 print("Accuracy:", accuracy_score(y, y_pred))
 print("Weights:", model.coef_)
 print("Bias:", model.intercept_)
+
+
+
+#Multilayer Perceptron
+from sklearn.datasets import load_iris
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+data = load_iris()
+
+X = data.data
+y = data.target
+
+# Multilayer Perceptron model
+model = MLPClassifier(
+    hidden_layer_sizes=(5,),
+    max_iter=3000,
+    random_state=42
+)
+
+# Train model
+model.fit(X, y)
+
+# Predictions
+y_pred = model.predict(X)
+
+# Results
+print("Accuracy:", accuracy_score(y, y_pred))
+print("Weights:", model.coefs_)
+print("Bias:", model.intercepts_)
